8deda3c1014d7a4b4b1f9847029a1e19da2ac023
Asif Alam <asifalam@Asifs-iMac-5.local>
2019-05-15 17:44:16 +0600

    model counter added

--- django_app/django_library/views.py
+++ django_app/django_library/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from author.models import Author
+from book.models import Book
+
 
 def home_page(request):
+
     template_name = 'home_page.html'
-    context = {}
+    total_author = Author.objects.all().count()
+    total_book = Book.objects.all().count()
+    context = {"total_author": total_author, "total_book": total_book}
     return render(request, template_name, context)
