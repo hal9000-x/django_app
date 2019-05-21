4d22aca0aa0b2500b3bd91999ce9af86528cc852
Asif Alam <asifalam@Asifs-iMac-5.local>
2019-05-22 01:11:36 +0600

    misc changes

--- django_app/author/models.py
+++ django_app/author/models.py
@@ -3,12 +3,25 @@ from django.db import models
 from django.utils import timezone
 from django.db.models import Q
 
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from django.utils.translation import gettext_lazy as _
+
 User = settings.AUTH_USER_MODEL
+
+def validate_email(value):
+    _validate_email = EmailValidator()
+
+    try:
+        _validate_email(value)
+    except ValidationError:
+        raise ValidationError(_('Enter a valid email address.'))
+    return value
 
 class Author(models.Model):
     name = models.CharField(max_length=120)
-    phone = models.CharField(max_length=15)
-    email = models.CharField(max_length=120)
+    phone = models.CharField(unique=True, max_length=15)
+    email = models.CharField(unique=True, max_length=120, validators=[validate_email])
     user = models.ForeignKey(User, default=1, null=True,
                              on_delete=models.SET_NULL)
 
@@ -29,3 +42,6 @@ class Author(models.Model):
 
     def get_delete_url(self):
         return f"{self.get_absolute_url()}/delete"
+
+
+
